7f03217bab372caa99f2aace359215cfa0cd5048
pravinboopathi <pravinboopathi1100@gmail.com>
2024-05-05 12:02:47 +0530

    Update

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+//Search bar
+function Search(){
+    return(
+      <div className="searchbar">
+        <input type="text" placeholder="Search" />
+      </div>
+    )
+  }
+
+  export default Search
